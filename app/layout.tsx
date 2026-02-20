d51c0a7ff176d02d87d4651aac6ce2d248b3bcdc
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-20 11:09:25 +0200

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import { Roboto } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -21,28 +18,28 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Note Hub",
     description: "Simple application designed for managing personal notes",
-    url: '/',
+    url: "/",
     images: [
       {
-        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
         width: 1200,
         height: 630,
-        alt: 'Note Hub preview'
-      }
-    ]
-  }
+        alt: "Note Hub preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
   children,
-  modal
+  modal,
 }: Readonly<{
-  children: React.ReactNode,
-  modal: React.ReactNode
+  children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${roboto.variable}`}>
         <TanStackProvider>
           <Header />
           {children}
