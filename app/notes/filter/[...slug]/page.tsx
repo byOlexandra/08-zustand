5b0aadd0a17daeecf6d94b506490e4288c6258bc
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-20 10:34:18 +0200

    Set generateMetadata function

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,10 +1,22 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
+import { Metadata } from "next";
 
 interface Props {
     params: Promise<{ slug: string[] }>
 }
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { slug } = await params;
+    const filterTag = slug[0] === 'all' ? "All notes" : slug[0];
+    return {
+        title: filterTag === "All notes" ? filterTag : `Notes by category: ${filterTag}`,
+        description: `Your notes in category : ${filterTag}`
+    }
+}
+
+
 
 export default async function Notes({ params }: Props) {
     const { slug } = await params;
