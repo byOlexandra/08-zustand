5db8ab8b68ae56b1fc57193b31563ae07e12e196
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-20 13:24:35 +0200

    Remove 'use client' directive in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,24 @@
-'use client'
+// 'use client'
 
 import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//     title: "Page not found - Note Hub",
-//     description: "Something wrong happened",
-//     openGraph: {
-//         title: "Page 404 - Note Hub",
-//         description: "Something wrong happened",
-//         url: '/',
-//         images: [
-//             {
-//                 url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-//                 width: 1200,
-//                 height: 630,
-//                 alt: '404 - Note Hub'
-//             }
-//         ]
-//     }
-// }
+export const metadata: Metadata = {
+    title: "Page not found - Note Hub",
+    description: "Something wrong happened",
+    openGraph: {
+        title: "Page 404 - Note Hub",
+        description: "Something wrong happened",
+        url: '/',
+        images: [
+            {
+                url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+                width: 1200,
+                height: 630,
+                alt: '404 - Note Hub'
+            }
+        ]
+    }
+}
 
 export default function NotFound() {
     return (
